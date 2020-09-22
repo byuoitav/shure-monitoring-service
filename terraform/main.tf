29e9faaf9d9c5dc5c30e1e5b23b2b3842c82fdbd
Brayden Winterton <bwinterton@gmail.com>
2020-09-22 11:30:21 -0600

    Update deployed image

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ module "shipyard_prd" {
   // required
   name           = "shure-monitoring-prd"
   image          = "docker.pkg.github.com/byuoitav/shure-monitoring-service/shure-monitoring-service-dev"
-  image_version  = "985e4ad"
+  image_version  = "a1e7745"
   container_port = 80 // doesn't actually exist in container
   repo_url       = "https://github.com/byuoitav/shure-monitoring-service"
 
