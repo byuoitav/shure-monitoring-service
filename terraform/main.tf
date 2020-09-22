b4b80a1474e758e82421b6e96cc0ca45a9d44bc3
Brayden Winterton <bwinterton@gmail.com>
2020-09-22 08:50:29 -0600

    Add changed s3 backend for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket     = "terraform-state-storage-586877430255"
-    lock_table = "terraform-state-lock-586877430255"
-    region     = "us-west-2"
+    bucket         = "terraform-state-storage-586877430255"
+    dynamodb_table = "terraform-state-lock-586877430255"
+    region         = "us-west-2"
 
     // THIS MUST BE UNIQUE
     key = "shure-monitoring.tfstate"
